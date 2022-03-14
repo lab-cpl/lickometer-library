710bf592da916b42601dff10cae0052e2a6ef96a
nicolasluarte <l.luarte@uc.cl>
2022-03-14 13:32:06 -0300

    better error checking in load_data

--- src/load_data.R
+++ src/load_data.R
@@ -11,22 +11,8 @@ library(validate)
 # loads data in a directory
 # adds date and time columns
 load_data <- function(path){
-	list.files(
-		   path = path,
-		   pattern = "_[0-9]+\\.csv",
-		   full.names = T
-		   ) %>%
-	set_names() %>%
-	purrr::map_dfr(
-			  read_csv, .id = "source"
-			  ) %>%
-	mutate(
-	       fecha = source_to_date(source),
-	       fecha_ms = source_to_date_posix_ms(source),
-	       tiempo_fecha_ms = fecha_ms + tiempo,
-	       tiempo_fecha = source_to_date_sec(source) + lubridate::seconds(tiempo / 1000)
-	       ) %>%
-	format_data_type(.)
+	load_data_as_char(path) %>%
+		validate_data()
 }
 # creates a column with date
 source_to_date <- function(x) {
@@ -57,15 +43,18 @@ source_to_date_posix_ms <- function(x) {
 format_data_type <- function(raw_file){
 			raw_file %>%
 				mutate(
-						ID = as.factor(ID),
-						sensor = as.factor(sensor),
-						tiempo = as.numeric(tiempo),
-						actividad = as.numeric(actividad),
-						evento = as.numeric(evento),
-						exito = as.factor(exito)
-			   )
+					ID = as.factor(ID),
+					sensor = as.factor(sensor),
+					tiempo = as.numeric(tiempo),
+					actividad = as.numeric(actividad),
+					evento = as.numeric(evento),
+					exito = as.factor(exito),
+				        fecha = source_to_date(source),
+				        fecha_ms = source_to_date_posix_ms(source),
+				        tiempo_fecha_ms = fecha_ms + tiempo,
+				        tiempo_fecha = source_to_date_sec(source) + lubridate::seconds(tiempo / 1000)
+	       )
 }
-
 load_data_as_char <- function(path){
 	list.files(
 		   path = path,
@@ -77,7 +66,6 @@ load_data_as_char <- function(path){
 			  read_csv, .id = "source", col_types = cols(.default = "c")
 			  )
 }
-
 validate_data <- function(data_as_char){
 	rules <- validate::validator(
 				     field_format(ID, "^[0-9]+$", type = "regex"),
@@ -87,9 +75,19 @@ validate_data <- function(data_as_char){
 				     field_format(evento, "^[0-9]+$", type = "regex"),
 				     field_format(exito, "^[0-9]+$", type = "regex")
 				     )
-	out <- confront(data_as_char, rules)
-	out_tibble <- summary(out)
-	out_tibble %>%
-		mutate(name = names(data_as_char[-1])) -> out_tibble
-	return(out_tibble)
+	format_errors <- confront(data_as_char, rules)$._value %>%
+		as_tibble()
+	error_matrix <- which(!as.matrix(format_errors), arr.ind = TRUE)
+	if (length(error_matrix) == 0){
+		message("Data contains no format errors")
+		return(format_data_type(data_as_char))
+	}
+	else{
+		message("Format errors found in the following rows and columns")
+		print(error_matrix)
+		return(error_matrix)
+	}
+	return(NA)
 }
+
+load_data("../test/files") -> aa
