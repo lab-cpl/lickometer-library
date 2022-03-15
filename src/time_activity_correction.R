0729a698726c9bc7c910d549d14f0c4b758a6113
Josefina Jara <josefinaijb@uc.cl>
2022-03-15 15:31:40 -0300

    Update time_activity_correction.R

--- src/time_activity_correction.R
+++ src/time_activity_correction.R
@@ -1,2 +1,30 @@
 # time_activity_correction
 # v 0
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+
+#import data from merged -csv
+#add time taken to complete activity and beginning of the program
+#compare time of activity to beginning and end time of the procedure
+#add a column that contains binary data for valid/non-valid rows (0: no 1:yes)
+
+#hora_inicio+fecha_ms < tiempo_fecha_ms < hora_termino+fecha_ms
+
+merged_data <-
+  data.frame(read.csv(
+    "~/GitHub/lickometer-library/test/files/merged_example.csv"
+  ))
+
+time_activity_correction <-
+  merged_data %>% mutate(
+    inicio_ms = as.integer(seconds(hms(hora_inicio))) + fecha_ms,
+    termino_ms = as.integer(seconds(hms(hora_termino))) + fecha_ms,
+    valido = ifelse(
+      inicio_ms <= tiempo_fecha_ms &
+        tiempo_fecha_ms < termino_ms,
+      "0",
+      "1"
+    )
+  )
