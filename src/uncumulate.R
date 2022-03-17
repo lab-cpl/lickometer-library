28d5d79ab82dc9336420f35f49cf271f9d026017
nicolasluarte <l.luarte@uc.cl>
2022-03-17 14:56:52 -0300

    working uncumulate

--- src/uncumulate.R
+++ src/uncumulate.R
@@ -2,7 +2,10 @@ uncumulate <- function(merged_input){
          merged_input %>%
                  group_by(ID, n_sesion, sensor) %>%
                  mutate(
-                        evento_no_acumulado = sum(abs(diff(evento)))
+                        evento_no_acumulado = c(0, diff(evento)) %>%
+				replace(. == 0, NA),
+                        actividad_no_acumulada = c(0, diff(actividad)) %>%
+				replace(. == 0, NA)
                         ) %>%
                  ungroup()
- }
+}
