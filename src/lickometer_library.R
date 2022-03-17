42188c3f7ed8f17d6e4d57ec05c534da26ba4a7b
nicolasluarte <l.luarte@uc.cl>
2022-03-17 14:50:28 -0300

    test script

--- src/lickometer_library.R
+++ src/lickometer_library.R
@@ -3,3 +3,18 @@ source("load_metadata.R")
 source("merge_inputs.R")
 source("time_activity_correction.R")
 source("uncumulate.R")
+
+# merge data and metadata
+merge_inputs(
+	     "../test/files/metadata_example.csv",
+	     "../test/files"
+	     ) %>%
+# creates a column with valid and invalid data 1: valid
+time_activity_correction() %>%
+# binary form of events
+uncumulate() -> data_final
+
+# create a csv file to check for possible errors
+data_final %>%
+	write_csv("../test/files/merged_example.csv")
+
