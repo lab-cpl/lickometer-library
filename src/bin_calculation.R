228f13b06fe2c5af3a978ae048d662465350113d
nicolasluarte <l.luarte@uc.cl>
2022-03-18 14:18:24 -0300

    bin function

---
+++ src/bin_calculation.R
@@ -0,0 +1,17 @@
+read_csv("../test/files/merged_example.csv") -> dd
+
+bin_calculation <- function(merged_data, bin_size_ms){
+	merged_data %>%
+		group_by(
+			 ID, n_sesion
+			 ) %>%
+		mutate(
+		       tiempo_relativo = tiempo - tiempo[1],
+		       bins = cut(
+				  tiempo_relativo,
+				  breaks = c(seq(0, max(tiempo_relativo), bin_size_ms)),
+				  labels = FALSE,
+				  include.lowest = TRUE
+				  )
+		       )
+}
