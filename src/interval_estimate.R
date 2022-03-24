2bf5be11f756ecc65408a7e88f5911957f5c3a37
nicolasluarte <l.luarte@uc.cl>
2022-03-24 17:12:10 -0300

    fix interval_estimate, add burst_estimate to lickometer_library

--- src/interval_estimate.R
+++ src/interval_estimate.R
@@ -1,6 +1,6 @@
 #interval estimate
 
 interval_estimate <- function(merged_input){
-  merged_input %>% group_by(ID,fecha,n_licometro) %>%
+  merged_input %>% group_by(ID,n_sesion,sensor) %>%
     mutate(interval_estimate = tiempo-lag(tiempo))
 }
