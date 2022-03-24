b3c7fa64762b728b63c6565fae725283284ecedb
nicolasluarte <l.luarte@uc.cl>
2022-03-24 20:12:12 -0300

    n_clusters update

--- src/n_clusters.R
+++ src/n_clusters.R
@@ -1,9 +1,8 @@
-x <- c(1,1,1,2,2,1,1,1,1,2,2,1,1)
 library(tidyverse)
 
 data<- read_csv("../test/files/merged_example.csv")
 
-counter <- function(x){
+make_groups <- function(x){
   counter = 0
   out = c(0)
   for (i in 1:length(x)){
@@ -22,26 +21,11 @@ counter <- function(x){
 
 
 n_clusters <- function(merged_input){
-  merged_input %>% mutate(tt=as.numeric(as.factor(cluster_bool))) %>% View
+	merged_input %>%
+		mutate(cluster_bool = cluster_bool %>%
+		       as.factor() %>%
+		       as.numeric()
+	       )
 }
 
-n_clusters(data)
-
-
-lag(x[4]) != x[4]
-
-counter = 0
-out = c()
-for (i in 1:length(x)){
-  if (i > 1){
-    if(x[i] != x[i-1]){
-      counter = counter + 1
-      out = append(out, counter)
-    }
-    else{
-      out = append(out, counter)
-    }    
-  }
-}
-out
-
+n_clusters(data) %>% select(cluster_bool) %>% max()
