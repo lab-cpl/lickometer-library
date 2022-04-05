e54cb08aa9a096506fad93fb822d0d60f777869d
nicolasluarte <l.luarte@uc.cl>
2022-04-05 15:09:40 -0400

    synch_lick_event first working function

--- src/synch_lick_event.R
+++ src/synch_lick_event.R
@@ -1,45 +1,65 @@
-library(tidyverse)
-
-x <- c(NA, 1, 1, NA, NA, 1, NA, NA , 1)
-y <- c(100, 150, 200, 330, 450, 600, 800, 950, 1000)
-ID <- c(111, 111, 222, 111, 222, 222, 222, 222, 222)
-n_sesion <- c(1, 1, 1, 1, 1, 1, 1, 1 , 1)
-sensor <- c(1, 1, 1, 1, 1, 1, 1, 1 , 1)
-tt <- rnorm(9 , 100, 1)
-ta <- tibble(x = x, y = y, ID = ID, n_sesion = n_sesion, sensor = sensor)
-
-peri_events <- function(merged_data, animal_number, n_sesion_, sensor_){
-	merged_data %>%
-		mutate(rn = row_number()) %>%
-		filter(x == 1,
-		       ID == animal_number,
-		       n_sesion == n_sesion_,
-		       sensor == sensor_
-		       ) %>%
-		select(rn) %>%
-		as.list() %>%
-		unlist() %>%
-		set_names(., paste0, "_ev") %>%
-		map_dfr(
-			function(x){
-				as.numeric((y - y[x]))
-			}
-			) %>%
-		bind_cols(., merged_data) %>%
-		nest(event_time_diff = contains("_ev")) %>%
-		mutate(
-		       rn = row_number()
-		       )
-#		map_dfr(function(x) {
-#				dd <- merged_data %>% filter(ID == ID)
-#				as.numeric(abs(dd$y - dd$y[x]))})
-#	bind_cols(., merged_data %>%
-#		  mutate(rn = row_number()) %>%
-#		  filter(ID == ID)) %>%
-#	nest(event_list = contains("_ev")) %>%
-#	mutate(ev = lapply(event_list, function(x) min(x))) %>%
-#	unnest(ev) %>%
-#	mutate(ev = if_else(ev <= window_ms, 1, 0)) -> out
-#	return(out)
+synch_lick_event <- function(ds){
+	ds <- ds %>% mutate(rn = row_number())
+ds %>%
+	group_split(
+		    ID,
+		    n_sesion,
+		    sensor
+		    ) %>%
+	map(., function(x) {
+		    # get all indices of triggered events per list
+		    x %>%
+			    filter(evento_no_acumulado == 1) %>%
+			    select(rn) -> event_indices
+		    x %>%
+			    select(rn) -> row_index
+		    # get a list per event
+		    # each list contains data from previous trigger up
+		    # to the next trigger
+		    # create list with these ranges
+		    event_indices %>%
+			    map(
+				.,
+				function(event_index){
+					tibble_list <- list()
+					# loop through each event index
+					for(i in 1:length(event_index)){
+						# if its the first event the
+						# starting point should be the
+						# first lick within conditions
+						if (event_index[i] == min(event_index)){
+							start_ = 1
+						}
+						# otherwise we take the
+						# previous event as starting
+						# point
+						else{
+							start_ = event_index[i-1]
+							start_ = which(row_index == start_)
+						}
+						# if its the last event then we
+						# take the last lick present in
+						# the list
+						if (event_index[i] == max(event_index)){
+							end_ = max(row_index)
+						}
+						# otherwise we just take the
+						# following event
+						else{
+							end_ = event_index[i+1]
+							end_ = which(row_index == end_)
+						}
+						# original indices are sliced
+						r <- slice(row_index, start_:end_)
+						d <- ds[unlist(r), ] %>%
+							mutate(
+							       tiempo_peri_evento = tiempo - tiempo[which(rn == event_index[i])]
+							       )
+						tibble_list <- append(tibble_list, list(d))
+					}
+					return(tibble_list)
+				}
+				)
+		    }) -> out
+	bind_rows(out) %>% unnest(rn)
 }
-peri_events(ta, 222, 1, 1) %>% unnest(event_time_diff)
