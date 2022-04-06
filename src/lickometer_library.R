c0abc0c6a59037f3d0d86dea7687c14f621f7611
nicolasluarte <l.luarte@uc.cl>
2022-04-06 00:28:52 -0400

    updated library

--- src/lickometer_library.R
+++ src/lickometer_library.R
@@ -1,9 +1,14 @@
 #libraries
-
-pacman::p_load(tidyr,tidyverse,dplyr,purrr,readr,lubridate,validate,ggplot2)
+pacman::p_load(tidyr,
+	       tidyverse,
+	       dplyr,
+	       purrr,
+	       readr,
+	       lubridate,
+	       validate,
+	       ggplot2)
 
 #lickometer_library
-
 source("load_data.R")
 source("load_metadata.R")
 source("merge_inputs.R")
@@ -14,6 +19,7 @@ source("bin_calculation.R")
 source("burst_estimates.R")
 source("n_clusters.R")
 source("pause_ms.R")
+source("synch_lick_event.R")
 
 # merge data and metadata
 merge_inputs(
@@ -29,6 +35,26 @@ bin_calculation(., 600000) %>%
 detect_bursts(., 1000) %>%
 n_clusters() %>%
 pause_ms() -> data_final
+
+# peri-event
+data_final %>%
+	synch_lick_event() -> peri_event
+
+# peri-event interval_estimate
+peri_event %>%
+	mutate(
+	       estimulo = if_else(sensor == 0 & estimulo_spout_1 == "sacarosa", "sacarosa", "agua")
+	       ) %>%
+	filter(interval_estimate > 100, interval_estimate <= 1000, estimulo == "sacarosa") %>%
+	ggplot(
+	       aes(
+		   tiempo_peri_evento,
+		   interval_estimate
+		   )
+	       ) +
+	facet_grid(evento~estimulo) +
+	theme_bw()
+
 
 
 # create a csv file to check for possible errors
