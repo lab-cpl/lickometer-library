d6b9f7654e323fdce6dfceacb6c0625486a52f04
nicolasluarte <l.luarte@uc.cl>
2022-04-07 21:42:09 -0400

    better synch lick event

--- src/synch_lick_event.R
+++ src/synch_lick_event.R
@@ -1,65 +1,35 @@
 synch_lick_event <- function(ds){
-	ds <- ds %>% mutate(rn = row_number())
+plan(multisession, workers = 4)
+ds %>%
+	mutate(rn = row_number()) -> ds
+ds %>% select(rn) -> row_index
 ds %>%
 	group_split(
 		    ID,
 		    n_sesion,
 		    sensor
 		    ) %>%
-	map(., function(x) {
-		    # get all indices of triggered events per list
-		    x %>%
-			    filter(evento_no_acumulado == 1) %>%
-			    select(rn) -> event_indices
-		    x %>%
-			    select(rn) -> row_index
-		    # get a list per event
-		    # each list contains data from previous trigger up
-		    # to the next trigger
-		    # create list with these ranges
-		    event_indices %>%
-			    map(
-				.,
-				function(event_index){
-					tibble_list <- list()
-					# loop through each event index
-					for(i in 1:length(event_index)){
-						# if its the first event the
-						# starting point should be the
-						# first lick within conditions
-						if (event_index[i] == min(event_index)){
-							start_ = 1
-						}
-						# otherwise we take the
-						# previous event as starting
-						# point
-						else{
-							start_ = event_index[i-1]
-							start_ = which(row_index == start_)
-						}
-						# if its the last event then we
-						# take the last lick present in
-						# the list
-						if (event_index[i] == max(event_index)){
-							end_ = max(row_index)
-						}
-						# otherwise we just take the
-						# following event
-						else{
-							end_ = event_index[i+1]
-							end_ = which(row_index == end_)
-						}
-						# original indices are sliced
-						r <- slice(row_index, start_:end_)
-						d <- ds[unlist(r), ] %>%
-							mutate(
-							       tiempo_peri_evento = tiempo - tiempo[which(rn == event_index[i])]
-							       )
-						tibble_list <- append(tibble_list, list(d))
-					}
-					return(tibble_list)
-				}
-				)
-		    }) -> out
-	return(bind_rows(out))
+	map_dfr(., function(x){
+			x %>%
+				filter(evento_no_acumulado == 1) %>%
+				select(rn) -> event_indices
+		       tibble(
+			      start_ = lag(unlist(event_indices), default = min(row_index)),
+			      end_ = lead(unlist(event_indices), default = max(row_index)),
+			      idx = event_indices
+			      )
+}) -> out
+	return(
+	bind_rows(
+	as.list(as.data.frame(t(out))) %>%
+		future_map(., function(x){
+				rows <- unlist(c(slice(row_index, x[1]:x[2])))
+				ds[rows,] %>%
+					mutate(
+					       event_id = x[3],
+					       tiempo_peri_evento = tiempo - tiempo[which(rn == x[3])]
+					) %>% filter(sensor == ds$sensor[x[3]])
+			      })
+	)
+	)
 }
